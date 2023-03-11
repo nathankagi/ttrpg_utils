3c3bb287824df695f681cfe7090ed94020e34418
Nathan Kagi <npkagi@outlook.com>
2023-03-11 11:34:58 +0800

    using pocketbase sdk

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -1,7 +1,24 @@
+import PocketBase from 'pocketbase';
+
+export const dynamic = 'auto',
+    dynamicParams = true,
+    revalidate = 0,
+    fetchCache = 'auto',
+    runtime = 'nodejs',
+    preferredRegion = 'auto'
+
 async function getCharacters() {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/character/records?page=1&perPage=30');
-    const data = await res.json();
+    // using pocketbase sdk
+    const db = new PocketBase('http://127.0.0.1:8090');
+    const data = await db.collection('character').getList(1, 30)
+    // const data = await db.records.getList('characters');
     return data?.items as any[];
+
+    // using fetch api
+    // const res = await fetch('http://127.0.0.1:8090/api/collections/character/records?page=1&perPage=30',
+    //     { cache: 'no-store' });
+    // const data = await res.json();
+    // return data?.items as any[];
 }
 
 // server component by default
@@ -20,7 +37,6 @@ export default async function TestCharacterPage() {
 
 function Card({ ...character }: any) {
     const { id, name, description, created, updated } = character || {};
-    console.log(character);
     return (
         <div className="p-10">
             <div>I AM A CARD</div>
