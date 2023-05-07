8b3473ed9cbf2507a635bed1fdc8154ffaa4e7d1
Nathan Kagi <npkagi@outlook.com>
2023-05-07 10:51:49 +0800

    fixed router load bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
+import NavBar from '../components/NavBar'
+import { NavBarIcon } from '../components/MenuIcons'
+import SideBar from '../components/SideBar'
 
-import NavBar from "../components/NavBar";
-import SideBar from "../components/SideBar";
-import { NavBarIcon } from '../components/MenuIcons';
-import { FaBars } from "react-icons/fa";
-
-import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa'
 
 export const metadata = {
   title: 'Next.js',
@@ -18,18 +16,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-      <NavBar>
-        <NavBarIcon text="Home" to={'/'}></NavBarIcon>
-        <NavBarIcon text="Tools" to={undefined}></NavBarIcon>
-        <NavBarIcon icon={<FaBars />} to={undefined}>
-          {
-            // dropdown state here
-          }
-        </NavBarIcon>
-      </NavBar>
-      <SideBar />
-      <body>{children}</body>
-    </>
+    <html lang="en">
+      <body>
+        <NavBar>
+          <NavBarIcon text="Home" to={'/'}></NavBarIcon>
+          <NavBarIcon text="Tools" to={undefined}></NavBarIcon>
+          <NavBarIcon icon={<FaBars />} to={undefined}>
+            {
+              // dropdown state here
+            }
+          </NavBarIcon>
+        </NavBar>
+        <SideBar />
+        {children}</body>
+    </html>
   )
 }
