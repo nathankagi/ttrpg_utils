89b37a25d3c2ffe6c79f7fbc5db2abd7550653f8
Nathan Kagi <npkagi@outlook.com>
2023-05-24 20:32:43 +0800

    page layout, fix z index for dropdown menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
             <body>
                 <NavBar />
                 <SideBar />
-                {children}
+                <div className="page">
+                    {children}
+                </div>
             </body>
         </html>
     );
