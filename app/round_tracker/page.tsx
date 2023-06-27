bf9fb2bc4366b556cde90152be6aaf5201851491
Nathan Kagi <npkagi@outlook.com>
2023-06-27 15:52:27 +0300

    scrollable items

--- app/round_tracker/page.tsx
+++ app/round_tracker/page.tsx
@@ -3,26 +3,76 @@
 import { useState } from "react";
 import { FaBars, FaCog, FaTools } from "react-icons/fa"; // font awesome 5
 
-export default function RoundTracker() {
-  let items = ["item1", "item2", "item3", "item4"];
+const Tracker = () => {
+  return (
+    <div className="flex flex-row p-4">
+      <RoundTracker></RoundTracker>
+      <ItemList></ItemList>
+    </div>
+  );
+};
+
+const RoundTracker = ({ ...props }) => {
+  let [active, setActive] = useState("");
+  //   let [items, setItems] = useState([]);
+
+  let items = ["item1", "item2", "item3", "item4", "item5"];
 
   return (
-    <div className=" w-96 border-black border-2 rounded-lg">
-      <div className="rounded-md p-2 m-5 h-24 bg-white border-2 border-black text-black"></div>
+    <div className="w-2/5 border-black border-2 border-b-4 rounded-lg m-2">
+      <div className="rounded-md p-2 m-5 h-24 bg-white border-2 border-b-4 border-black text-black"></div>
       <TrackerMenuBar></TrackerMenuBar>
-      <div>
+      <div className="max-h-96 overflow-auto my-4">
         {items.map((item) => {
           return <RoundTrackerItem item={item}></RoundTrackerItem>;
         })}
       </div>
     </div>
   );
-}
+};
 
 const RoundTrackerItem = ({ item, ...props }) => {
-  return <div className="tracker_item">{item}</div>;
+  return (
+    <div className="flex align-middle rounded-md p-2 border-b-4 m-5 h-16 bg-white border-2 border-black text-black hover:bg-gray-200 transition-all">
+      {item}
+    </div>
+  );
 };
 
 const TrackerMenuBar = ({ ...props }) => {
-  return <div className="w-fill h-10"></div>;
+  const handleReset = () => {
+    // set to default state
+  };
+
+  const handleNextItem = () => {
+    // change state of round tracker
+  };
+
+  return (
+    <div className="flex flex-row h-12 rounded-lg m-5 items-center justify-center bg-gray-200">
+      <div
+        onClick={handleNextItem}
+        className="w-36 h-8 mx-2 bg-white border-2 border-transparent rounded-lg flex justify-center text-center items-center hover:border-black transition-all duration-300 ease-in-out"
+      >
+        NEXT TURN
+      </div>
+      <div className="flex-grow"></div>
+      <div className="m-2 w-8 h-8 text-lg border-transparent rounded-lg border-2 flex items-center justify-center bg-white hover:border-black transition-all duration-300 ease-in-out">
+        <FaCog></FaCog>
+      </div>
+    </div>
+  );
 };
+
+const ItemList = () => {
+  return (
+    <div className="w-2/5 border-black border-2 border-b-4 rounded-lg m-2">
+      <div className="rounded-md p-2 m-5 h-24 bg-white border-2 border-b-4 border-black text-black">
+        MENU
+      </div>
+      <div className="rounded-md p-2 m-5 h-3/4 bg-white border-2 border-b-4 border-black text-black"></div>
+    </div>
+  );
+};
+
+export default Tracker;
--- components/MenuIcons.tsx
+++ components/MenuIcons.tsx
@@ -6,145 +6,144 @@ import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { CSSTransition } from "react-transition-group";
 
 export const SideBarIcon = ({
-    icon,
-    text = "",
-    link = "https://www.google.com/",
+  icon,
+  text = "",
+  link = "https://www.google.com/",
 }) => {
-    return (
-        <a href={link}>
-            <div className="sidebar-icon group">
-                {icon}
-                <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
-            </div>
-        </a>
-    );
+  return (
+    <a href={link}>
+      <div className="sidebar-icon group">
+        {icon}
+        <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+      </div>
+    </a>
+  );
 };
 
 export const NavBarIcon = ({ ...props }) => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleMouseEnter = () => {
-        setOpen(true);
-    };
+  const handleMouseEnter = () => {
+    setOpen(true);
+  };
 
-    const handleMouseLeave = () => {
-        setOpen(false);
-    };
+  const handleMouseLeave = () => {
+    setOpen(false);
+  };
 
-    return (
-        <div
-            onMouseLeave={handleMouseLeave}>
-            <Link
-                className="navbar-icon font-sans"
-                href={props.to ? props.to : ""}
-                onMouseEnter={handleMouseEnter}
-            >
-                {props.text ? props.text : props.icon}
-            </Link>
-            {open && props.children}
-        </div>
-    );
+  return (
+    <div onMouseLeave={handleMouseLeave}>
+      <Link
+        className="navbar-icon font-sans"
+        href={props.to ? props.to : ""}
+        onMouseEnter={handleMouseEnter}
+      >
+        {props.text ? props.text : props.icon}
+      </Link>
+      {open && props.children}
+    </div>
+  );
 };
 
 // consolidate dropdown features into a generic dropdown component
 export const ToolsDropdown = (props) => {
-    const nodeRef = React.useRef(null);
-    const [activeMenu, setActiveMenu] = useState("main");
+  const nodeRef = React.useRef(null);
+  const [activeMenu, setActiveMenu] = useState("main");
 
-    const DropdownItem = (props) => {
-        return (
-            <Link
-                href={props.href ? props.href : ""}
-                className="menu-item"
-                onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
-            >
-                {props.children}
-            </Link>
-        );
-    };
-
+  const DropdownItem = (props) => {
     return (
-        <div className="dropdown">
-            <div className="menu">
-                <DropdownItem href="./round_tracker">Tracker</DropdownItem>
-                <DropdownItem href="./characters">Characters</DropdownItem>
-                <DropdownItem href="#">Test</DropdownItem>
-            </div>
-        </div >
+      <Link
+        href={props.href ? props.href : ""}
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        {props.children}
+      </Link>
     );
+  };
+
+  return (
+    <div className="dropdown">
+      <div className="menu">
+        <DropdownItem href="./round_tracker">Tracker</DropdownItem>
+        <DropdownItem href="./characters">Characters</DropdownItem>
+        <DropdownItem href="#">Test</DropdownItem>
+      </div>
+    </div>
+  );
 };
 
 export const SettingsDropdown = (props) => {
-    const nodeRef = React.useRef(null);
-    const [activeMenu, setActiveMenu] = useState("main");
+  const nodeRef = React.useRef(null);
+  const [activeMenu, setActiveMenu] = useState("main");
 
-    const DropdownItem = (props) => {
-        return (
-            <Link
-                href="#"
-                className="menu-item"
-                onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
-            >
-                {props.children}
-            </Link>
-        );
-    };
-
+  const DropdownItem = (props) => {
     return (
-        <div className="dropdown">
-            <CSSTransition
-                in={activeMenu === "main"}
-                unmountOnExit
-                timeout={1}
-                classNames="menu-primary"
-                nodeRef={nodeRef}
-            >
-                <div ref={nodeRef}>
-                    <div className="menu">
-                        <DropdownItem goToMenu="user">User</DropdownItem>
-                        <DropdownItem goToMenu="settings">Settings</DropdownItem>
-                        <DropdownItem>Test</DropdownItem>
-                    </div>
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === "user"}
-                unmountOnExit
-                timeout={1}
-                classNames="menu-secondary"
-                nodeRef={nodeRef}
-            >
-                <div ref={nodeRef}>
-                    <div className="menu">
-                        <DropdownItem goToMenu="main">
-                            <FaArrowAltCircleLeft />
-                        </DropdownItem>
-                        <DropdownItem>Test</DropdownItem>
-                        <DropdownItem>Test</DropdownItem>
-                        <DropdownItem>Test</DropdownItem>
-                    </div>
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === "settings"}
-                unmountOnExit
-                timeout={1}
-                classNames="menu-secondary"
-                nodeRef={nodeRef}
-            >
-                <div ref={nodeRef}>
-                    <div className="menu">
-                        <DropdownItem goToMenu="main">
-                            <FaArrowAltCircleLeft />
-                        </DropdownItem>
-                        <DropdownItem>User</DropdownItem>
-                        <DropdownItem>Application Settings</DropdownItem>
-                        <DropdownItem>Privacy</DropdownItem>
-                    </div>
-                </div>
-            </CSSTransition>
-        </div>
+      <Link
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        {props.children}
+      </Link>
     );
+  };
+
+  return (
+    <div className="dropdown">
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={1}
+        classNames="menu-primary"
+        nodeRef={nodeRef}
+      >
+        <div ref={nodeRef}>
+          <div className="menu">
+            <DropdownItem goToMenu="user">User</DropdownItem>
+            <DropdownItem goToMenu="settings">Settings</DropdownItem>
+            <DropdownItem>Test</DropdownItem>
+          </div>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "user"}
+        unmountOnExit
+        timeout={1}
+        classNames="menu-secondary"
+        nodeRef={nodeRef}
+      >
+        <div ref={nodeRef}>
+          <div className="menu">
+            <DropdownItem goToMenu="main">
+              <FaArrowAltCircleLeft />
+            </DropdownItem>
+            <DropdownItem>Test</DropdownItem>
+            <DropdownItem>Test</DropdownItem>
+            <DropdownItem>Test</DropdownItem>
+          </div>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "settings"}
+        unmountOnExit
+        timeout={1}
+        classNames="menu-secondary"
+        nodeRef={nodeRef}
+      >
+        <div ref={nodeRef}>
+          <div className="menu">
+            <DropdownItem goToMenu="main">
+              <FaArrowAltCircleLeft />
+            </DropdownItem>
+            <DropdownItem>User</DropdownItem>
+            <DropdownItem>Application Settings</DropdownItem>
+            <DropdownItem>Privacy</DropdownItem>
+          </div>
+        </div>
+      </CSSTransition>
+    </div>
+  );
 };
